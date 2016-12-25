a6a15db2b8b6ecfb5d6b7e0c4418d3677116ad8b
Urban Cvek <urban.cvek@shopster.io>
2016-12-25 12:31:16 +0100

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { autobind } from 'core-decorators';
 import React, { Component } from 'react';
+import { BrowserRouter, Match, Link } from 'react-router';
+
+import HomePage from 'pages/HomePage';
+import AboutPage from 'pages/AboutPage';
 
 @autobind
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello World!
-      </div>
+      <BrowserRouter>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+
+          <Match exactly pattern="/" component={HomePage} />
+          <Match pattern="/about" component={AboutPage} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
